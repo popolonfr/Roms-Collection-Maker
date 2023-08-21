0149d7e7d3473827369f82ce632c37a0c217d3e3
popolonfr <141568260+popolonfr@users.noreply.github.com>
2023-08-21 02:04:39 +0200

    Add files via upload

---
+++ 2- Build the Rom.bat
@@ -0,0 +1,14 @@
+@ECHO OFF
+REM zasm %~n1.asm %~n1.cmp
+
+IF NOT EXIST ".\Build\EditThisList.asm" (GOTO :EOF)
+
+COPY /b .\Roms\*.rom .\Build\Roms.tmp
+ZASM Data2 .\Build\Data.tmp
+COPY /b Data1+.\Build\data.tmp+.\Build\Roms.tmp .\Build\LoadThis.rom
+::COPY /b Data1++.\Build\data.tmp .\Build\LoadThis.rom
+DEL *.lst
+DEL .\Build\*.tmp
+
+PAUSE
+@ECHO ON
