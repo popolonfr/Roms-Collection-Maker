8843e8f85d6c5ecfe16d022ea09a89a94eafb504
popolonfr <141568260+popolonfr@users.noreply.github.com>
2023-09-24 23:05:46 +0200

    Update 2- Build the Rom.bat

--- 2- Build the Rom.bat
+++ 2- Build the Rom.bat
@@ -1,12 +1,10 @@
 @ECHO OFF
-REM zasm %~n1.asm %~n1.cmp
 
 IF NOT EXIST ".\Build\EditThisList.asm" (GOTO :EOF)
 
 COPY /b .\Roms\*.rom .\Build\Roms.tmp
 ZASM Data2 .\Build\Data.tmp
 COPY /b Data1+.\Build\data.tmp+.\Build\Roms.tmp .\Build\LoadThis.rom
-::COPY /b Data1++.\Build\data.tmp .\Build\LoadThis.rom
 DEL *.lst
 DEL .\Build\*.tmp
 
