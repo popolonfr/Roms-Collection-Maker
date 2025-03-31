cad787f2b68ef2894b8a51757a2e1d75f44e4852
popolonfr <141568260+popolonfr@users.noreply.github.com>
2025-03-31 14:19:10 +0200

    Delete RCM Title.asm

--- RCM Title.asm
+++
@@ -1,4 +0,0 @@
-; Give your Rom compilation a title. It will be displayed at the top of the screen just above the list of Roms
-; You must respect the number of characters between quotation marks which is 40
-
-	db	"         My games compilation 1         "
