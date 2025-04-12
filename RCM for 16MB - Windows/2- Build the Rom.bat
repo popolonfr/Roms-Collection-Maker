e56f89df92f78aa86c8aac536f4d89ae91ea8df6
popolonfr <141568260+popolonfr@users.noreply.github.com>
2025-04-12 23:46:17 +0200

    Add files via upload

---
+++ RCM for 16MB - Windows/2- Build the Rom.bat
@@ -0,0 +1,13 @@
+@ECHO OFF
+
+IF NOT EXIST ".\Build\EditThisList.asm" CALL "1- Build the List.bat" && @ECHO OFF
+IF %ERRORLEVEL% NEQ 0 EXIT
+
+.\Zasm\ZASM -w ".\Sources\Menu\RCM Menu.asm" "RCM Menu.bin"
+COPY /b "RCM Menu.bin"+".\Roms\*.rom" ".\Build\LoadThis.rom"
+
+DEL "RCM Menu.lst"
+DEL "RCM Menu.bin"
+
+PAUSE
+@ECHO ON
